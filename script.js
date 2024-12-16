ee26e6c5d6e27afb0c84e674604d95588b77f9e6
Golden7kihong <rlghd0102@gmail.com>
2024-12-17 00:30:19 +0900

    12312231
    123123123123123213

--- script.js
+++ script.js
@@ -1469,6 +1469,67 @@ overlay.addEventListener("click", () => {
     closeChaeModal();
   }
 });
+
+// "노인과바다" 모달 관련 변수
+const oldModal = document.getElementById("노인과바다");
+const oldOpenButton = document.getElementById("노인과바다openModal");
+
+// 노인과바다 모달관련
+function openOldModal() {
+  overlay.style.display = "block";
+  oldModal.style.display = "block";
+  lockScroll(); // 스크롤 잠금
+
+  gsap.fromTo(
+    overlay,
+    { opacity: 0 },
+    { opacity: 1, duration: 0.5, ease: "power2.out" }
+  );
+
+  gsap.fromTo(
+    oldModal,
+    { opacity: 0, y: "100%" },
+    {
+      duration: 0.5,
+      opacity: 1,
+      y: "10%",
+      ease: "power2.out"
+    }
+  );
+}
+
+function closeOldModal() {
+  gsap.to(oldModal, {
+    duration: 0.5,
+    opacity: 0,
+    y: "100%",
+    ease: "power2.in",
+    onComplete: () => {
+      oldModal.style.display = "none";
+    }
+  });
+
+  gsap.to(overlay, {
+    duration: 0.5,
+    opacity: 0,
+    ease: "power2.in",
+    onComplete: () => {
+      overlay.style.display = "none";
+      unlockScroll(); // 스크롤 해제
+    }
+  });
+}
+
+oldOpenButton.addEventListener("click", openOldModal);
+
+overlay.addEventListener("click", () => {
+  if (isFloatModalOpen) {
+    closeFloatModal();
+  } else {
+    closeOldModal();
+  }
+});
+
 // 넘어가기 방지  넘어가기 방지  넘어가기 방지  넘어가기 방지  넘어가기 방지  넘어가기 방지  넘어가기 방지 //
 // 넘어가기 방지  넘어가기 방지  넘어가기 방지  넘어가기 방지  넘어가기 방지  넘어가기 방지  넘어가기 방지 //
 // 넘어가기 방지  넘어가기 방지  넘어가기 방지  넘어가기 방지  넘어가기 방지  넘어가기 방지  넘어가기 방지 //
@@ -2224,6 +2285,40 @@ chaebookmark.addEventListener("click", function () {
     chaebookmark.src = "images/bookmark_empty.png"; // 빈 책갈피 이미지
   }
 });
+
+// 노인!!!!하트와 책갈피 관련 변수
+const oldheart = document.getElementById("노인-빈하트");
+const oldbookmark = document.getElementById("노인-빈책갈피");
+const oldpurchaseButton = document.getElementById("노인과바다-구매");
+// 초기 상태 설정
+let isoldHeartFilled = false; // 하트 상태 (빈하트/꽉찬하트)
+let isoldBookmarkFilled = false; // 책갈피 상태 (빈책갈피/꽉찬책갈피)
+
+// 하트 클릭 이벤트
+oldheart.addEventListener("click", function () {
+  isoldHeartFilled = !isoldHeartFilled; // 상태 토글
+
+  // 하트 이미지 변경
+  if (isoldHeartFilled) {
+    oldheart.src = "images/heart_fill.png"; // 꽉찬 하트 이미지
+    oldpurchaseButton.style.display = "block"; // 구매 버튼 보이기
+  } else {
+    oldheart.src = "images/heart_empty.png"; // 빈 하트 이미지
+    oldpurchaseButton.style.display = "none"; // 구매 버튼 숨기기
+  }
+});
+
+// 책갈피 클릭 이벤트
+oldbookmark.addEventListener("click", function () {
+  isoldBookmarkFilled = !isoldBookmarkFilled; // 상태 토글
+
+  // 책갈피 이미지 변경
+  if (isoldBookmarkFilled) {
+    oldbookmark.src = "images/bookmark_fill.png"; // 꽉찬 책갈피 이미지
+  } else {
+    oldbookmark.src = "images/bookmark_empty.png"; // 빈 책갈피 이미지
+  }
+});
 // 넘어가기 방지  넘어가기 방지  넘어가기 방지  넘어가기 방지  넘어가기 방지  넘어가기 방지  넘어가기 방지 //
 // 넘어가기 방지  넘어가기 방지  넘어가기 방지  넘어가기 방지  넘어가기 방지  넘어가기 방지  넘어가기 방지 //
 // 넘어가기 방지  넘어가기 방지  넘어가기 방지  넘어가기 방지  넘어가기 방지  넘어가기 방지  넘어가기 방지 //
@@ -2547,3 +2642,19 @@ chaeBookmarkButton.addEventListener("click", function () {
     chaeBookmarkImage.style.display = "block"; // 숨기기
   }
 });
+
+// 노인과바다 북마크관련
+const oldBookmarkButton = document.getElementById("노인-빈책갈피");
+
+// 숨겨진 북마크 이미지 가져오기
+const oldBookmarkImage = document.getElementById("노인과바다bookmark");
+
+// 클릭 이벤트 추가
+oldBookmarkButton.addEventListener("click", function () {
+  if (oldBookmarkImage.style.display === "block") {
+    oldBookmarkImage.style.display = "none"; // 보이기
+  } else {
+    oldBookmarkImage.style.display = "block"; // 숨기기
+  }
+});
+
